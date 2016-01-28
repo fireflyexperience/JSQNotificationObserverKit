31cc7aa1caab52e554ac9a087e7b43af80a25efe
Andy Bennett <andy@steamshift.com>
2016-01-28 13:53:00 +0000

    Convenience additions

---
+++ Source/NotificationObserverAdditions.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+public struct JSQNotify {
+    public static func Signal(name: Swift.String) -> Notification<Any?, AnyObject> {
+        return Notification<Any?, AnyObject>(name: name)
+    }
+    
+    public static func String(name: Swift.String) -> Notification<Swift.String, AnyObject> {
+        return Notification<Swift.String, AnyObject>(name: name)
+    }
+}
+
+extension NotificationObserver
+{
+    public static func Signal(
+        name: Swift.String,
+        queue: NSOperationQueue? = nil,
+        center: NSNotificationCenter = .defaultCenter(),
+        handler: NotificationHandler) -> NotificationObserver<Any?, AnyObject>
+    {
+        let notification = JSQNotify.Signal(name)
+        return NotificationObserver<Any?, AnyObject>(notification: notification, queue: queue, center: center, handler: handler)
+    }
+}
